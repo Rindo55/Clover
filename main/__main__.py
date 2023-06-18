fdd4fcb64eda67e1cbe7f05e999f373d1625aa7e
Rindo55 <77073061+Rindo55@users.noreply.github.com>
2023-06-18 23:58:04 +0530

    Update __main__.py

--- main/__main__.py
+++ main/__main__.py
@@ -13,6 +13,46 @@ loop = asyncio.get_event_loop()
 async def start(bot, message: Message):
   return await message.reply_text("⚡ **Bot Is up...**")
 
+@app.on_message(filters.command(["send"]))
+async def start(bot, message: Message):
+        source_text="https://da.gd/KUOqvg"
+        golink = "https://da.gd/XHf7sT"
+        repl_markup=InlineKeyboardMarkup(
+
+            [
+
+                [
+
+                     InlineKeyboardButton(
+
+                        text="🐌TG FILE",
+
+                        url=source_text,
+
+                    ),
+
+                     InlineKeyboardButton(
+
+                          text="🚀GoFile",
+
+                          url=golink,
+
+                    ),
+
+                ],
+
+            ],
+
+        )
+        KAYO_ID = -1001159872623
+        orgtext = f"**#Encoded_File**" + "\n" + f"**‣ File Name**: `Demon Slayer S4 - 11 [720p.x265] @animxt.mkv`" + "\n" + "**‣ Video**: `720p HEVC x265 10Bit`" + "\n" + "**‣ Audio**: `Japanese`" + "\n" + f"**‣ Subtitle**: `English`" + "\n" + f"**‣ File Size**: `252.7 MiB`" + "\n" + f"**‣ Duration**: 55 Minutes 29 Seconds" + "\n" + f"**‣ Downloads**: [🔗Telegram File]({source_text})"
+        rep_id = 31516
+        return await app.send_message(
+              chat_id=KAYO_ID,
+              text=orgtext,
+              reply_markup=repl_markup,
+              reply_to_message_id=rep_id
+          )
 async def start_bot():
   print("==================================")
   print("[INFO]: AutoAnimeBot Started Bot Successfully")
